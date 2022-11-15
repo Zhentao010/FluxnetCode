d3299e4152e64d9cc8a58ba75c1351dd70f4a108
ZhentaoLIU <12232256@mail.sustech.edu.cn>
2022-11-15 21:20:04 +0800

    添加了读取VPD数据的功能

--- pycode/AllDataRead.py
+++ pycode/AllDataRead.py
@@ -15,15 +15,16 @@ import xlwt
 def dataTransfer(fileN):
     eco = pd.read_csv(fileN);
     ecom = len(eco);
-    Data = np.zeros((ecom,5),dtype = float);
+    Data = np.zeros((ecom,6),dtype = float);
     n=0;
     for i1 in range(ecom):
         if eco['RECO_NT_VUT_REF'][i1] != -9999:
             Data[n][0] = eco['TIMESTAMP_START'][i1];
             Data[n][1] = eco['TA_F_MDS'][i1];
-            Data[n][2] = eco['RECO_NT_VUT_REF'][i1];
-            Data[n][3] = 1000/(eco['TA_F_MDS'][i1]+273);
-            Data[n][4] = np.log(eco['RECO_NT_VUT_REF'][i1]);
+            Data[n][2] = eco['VPD_F_MDS'][i1];
+            Data[n][3] = eco['RECO_NT_VUT_REF'][i1];
+            Data[n][4] = 1000/(eco['TA_F_MDS'][i1]+273);
+            Data[n][5] = np.log(eco['RECO_NT_VUT_REF'][i1]);
             n = n+1;
         else:
             continue;
@@ -44,7 +45,7 @@ def save(data, path):
 path = 'D:/Data/fluxnet/OriginalData/AllHourlyData/';  #储存原始数据的位置
 for csv_file in os.listdir(path):
     a = csv_file[4:10];
-    path1 = 'C:/Users/Lenovo/Desktop/ALLvalue/' + a + '.xlsx';
+    path1 = 'C:/Users/Lenovo/Desktop/未划分年份的初级数据/RespTempVpd/' + a + '.xlsx';
     Data = dataTransfer(path + csv_file);
     Data_pd = pd.DataFrame(Data);
     writer = pd.ExcelWriter(path1);
