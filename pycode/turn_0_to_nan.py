004ad8ebaeb7f0a46f5ac552186342f140b7b152
ZhentaoLIU <12232256@mail.sustech.edu.cn>
2022-11-27 17:07:40 +0800

    新的代码，turn_0_to_nan,将矩阵中的0值去掉，并将矩阵缩小

---
+++ pycode/turn_0_to_nan.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 15:52:10 2022
+
+@author: Lenovo
+"""
+
+import pandas as pd
+import numpy as np
+
+path = 'C:/Users/Lenovo/Desktop/AverMinus (1).csv'
+
+data = pd.read_csv(path);
+
+[m,n] = data.shape;
+
+
+cal = np.zeros((1,n));
+
+for i in range(n):
+    for j in range(m):
+        if data[str(i+1)][j] != 0:
+            cal[0][i] = cal[0][i] + 1;
+
+b = [];
+for i in range(n):
+    b.append(cal[0][i]);
+    
+mm = max(b);
+
+a0 = np.zeros((int(mm)+1,n));
+
+for i in range(n):
+    a0[0][i] = i+1;
+
+for i in range(n):
+    nn = 0;
+    for j in range(m):
+        if data[str(i+1)][j] != 0:
+            nn = nn + 1;
+            a0[nn][i] = data[str(i+1)][j];
+            
+
+
+a = np.zeros((int(mm)+1,n));
+for i in range(n):
+    a[0][i] = i+1;
+
+for i in range(n):
+    for j in range(int(mm)):
+        if a0[j+1][i] != 0:
+            a[j+1][i] = a0[j+1][i];
+        else:
+            a[j+1][i] = 'nan';
+
+path00 = 'C:/Users/Lenovo/Desktop/AverMinus(1)0_to_nan.csv'
+a_np = pd.DataFrame(a);
+a_np.to_csv(path00, index= False, header= False)
